e48b182b2d270bea169240d1414c5dc37a8f02cb
FennSternberg <94621711+FennSternberg@users.noreply.github.com>
2022-02-13 18:26:27 +0000

    Add files via upload

--- wordle_bot/analyse_performance.py
+++ wordle_bot/analyse_performance.py
@@ -6,11 +6,13 @@ import matplotlib.pyplot as plt
 class Results:
     def __init__(self, scores_file, dictionary_file):
         self.scores = np.asarray(read_results(scores_file))
-        self.dictionary = pd.read_csv(dictionary_file)['word'].tolist()  # will be in exact same order as self.scores
+        self.dictionary = pd.read_csv(dictionary_file, index_col=0)  # will be in exact same order as self.scores
         self.mean = np.mean(self.scores)
         self.median = np.median(self.scores)
         self.max = np.amax(self.scores)
         self.freq_dist = frequency_distribution(self.scores)
+        self.no_losers = len([i for i in self.scores if i > 6])  # how many games were lost
+        self.losers = find_losers(self.scores, self.dictionary)  # list of all the games that lost
 
     def running_mean(self, window):
         d = pd.Series(self.scores)
@@ -22,6 +24,8 @@ class Results:
         plt.xlabel("'true word' usage rank")
         plt.ylabel('Number of guesses')
         plt.plot(y)
+        plt.plot(self.mean * np.ones(len(self.scores)),color='black')
+        plt.text(2, self.mean+0.03, 'total mean = ' + str(np.round(self.mean,2)))
         return
 
     def plot_freq_dist(self):
@@ -44,4 +48,11 @@ def frequency_distribution(scores):
     return np.asarray((unique, counts)).T
 
 
-fwd = Results('fwd_results.txt', 'five_word_dictionary.csv')  # results of game with five word dictionary database
+def find_losers(scores, dictionary):
+    losers_index = [idx for idx, val in enumerate(scores) if val > 6]
+    losers = dictionary.loc[losers_index]['word'].tolist()
+    return losers
+
+
+fwd = Results('fwd_crane_results.txt', 'five_word_dictionary.csv')  # results of game with five word dictionary database
+pos = Results('possible_crane_results.txt','possible_dictionary.csv')
